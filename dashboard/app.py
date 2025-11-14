0fa12ca1b55806ec6345fe6eccb9bd483375a2cf
OptiPunk <walderdaniel480@gmail.com>
2025-11-14 12:51:43 +0100

    reusable pie-chart function added, requirements file updated, app file repaired

--- dashboard/app.py
+++ dashboard/app.py
@@ -13,6 +13,8 @@ from shiny.express import input, render, ui
 from ipyleaflet import Map, GeoJSON
 import folium
 
+# reusable pie_chart function
+from pie_chart import pie_chart_from_column
 # === Load population data ===
 # Expect columns: "Alter", "Männer", "Frauen", and optionally "Stadtteil"
 df_pyr = pd.read_excel("../Input/2022.xlsx")
@@ -130,21 +132,203 @@ with ui.layout_columns(fill=False):
 
 
 # === KPIs ===
+
 with ui.layout_column_wrap(fill=False):
+    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+        "Wohnberechtigte Bevölkerung"
+
+        @render.text
+        def population():
+            return "pop"
+
+    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+        "Bevölkerung am Ort der Hauptwohnung"
+
+        @render.text
+        def population_main():
+            return "200"
+
+    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+        "Bevölkerung am Ort der Nebenwohnung"
+
+        @render.text
+        def population_seconday():
+            return "100"
+
+with ui.layout_column_wrap(fill=False):
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Frauenanteil in %"
+
+        @render.text
+        def population_female_percentage():
+            total = bv.shape[0]
+            if total == 0:
+                return "Keine Daten"
+            women = bv[bv["Geschlecht"] == "w"].shape[0]
+            return f"{(women / total * 100):.1f} %"
+
+    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+        "Männeranteil in %"
+
+        @render.text
+        def population_male_percentage():
+            return "hellau2"
 
     with ui.value_box(showcase=icon_svg("ruler-vertical")):
         "Durchschnittsalter in Jahren"
 
         @render.text
-        def kpi_avg_age():
-            d = agg_by_age()
-            if d.empty or d["Alter"].isna().all():
-                return "–"
-            weights = (d["Männer"].abs() + d["Frauen"]).values
-            ages = d["Alter"].values
-            wsum = weights.sum()
-            avg = np.nan if wsum == 0 else float((ages * weights).sum() / wsum)
-            return f"{avg:.1f}"
+        def average_age():
+            return "hellau3"
+
+with ui.layout_column_wrap(fill=False):
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Anzahl an Geburten"
+
+        @render.text
+        def num_births():
+            return "hellau4"
+
+    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+        "Anzahl an Sterbefällen"
+
+        @render.text
+        def num_deaths():
+            return "hellau5"
+
+    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+        "Saldo Geburten und Sterbefälle"
+
+        @render.text
+        def saldo_birth_deaths():
+            return "hellau6"
+
+with ui.layout_column_wrap(fill=False):
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Zuzüge"
+
+        @render.text
+        def moved_in():
+            return "hellau7"
+
+    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+        "Fortzüge"
+
+        @render.text
+        def moved_out():
+            return "hellau8"
+
+    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+        "Salo Zu- und Fortzüge"
+
+        @render.text
+        def saldo_moved():
+            return "hellau9"
+
+with ui.layout_columns(fill=False):  
+    with ui.card():  
+        ui.card_header("Familienstand")
+        @render_plotly
+        def family_pie():
+            fig = pie_chart_from_column(bv, "Familienstand", top_n=8, title="")
+            return fig
+
+
+    with ui.card():  
+        ui.card_header("Religionszugehörigkeit")
+        @render_plotly
+        def religion_pie():
+            fig = pie_chart_from_column(bv, "Religion", top_n=8, title="")
+            return fig
+
+with ui.layout_columns(fill=False):  
+    with ui.card():  
+        ui.card_header("Bevölkerung nach Migrationshintergrund")
+        @render_plotly
+        def pop_migra_pie():
+            fig = pie_chart_from_column(bv, "Staatsangehörigkeit", top_n=8, title="")
+            return fig
+        
+    with ui.card():  
+        ui.card_header("Häufigstes Bezugsland von Personen mit Migrationshintergrund")
+        ui.p("Tortendiagramm here")
+
+with ui.layout_columns(fill=False):  
+    with ui.card():  
+        ui.card_header("Privathaushalte")
+        ui.p("Tortendiagramm here")
+
+    with ui.card():  
+        ui.card_header("Wohnungen")
+        ui.p("Boxdiagramm here")
+
+with ui.layout_columns(fill=False):  
+    with ui.card():  
+        ui.card_header("Sinus-Milieus")
+        ui.p("Tortendiagramm here")
+
+    with ui.card():  
+        ui.card_header("Gemeinderatswahl 2024")
+        ui.p("Balkendiagramm here")
+
+with ui.layout_column_wrap(fill=False):
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Sozialversicherungspflichtig Beschäftigte"
+
+        @render.text
+        def insurance_workforce():
+            return "hellau7"
+
+    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+        "Beschäftigungsquote in %"
+
+        @render.text
+        def workforce_percentage():
+            return "hellau8"
+
+    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+        "Arbeitslose absolut"
+
+        @render.text
+        def num_jobless():
+            return "hellau9"
+
+    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+        "Arbeitslosenquotient in %"
+
+        @render.text
+        def jobless_percentage():
+            return "hellau9"
+
+with ui.layout_column_wrap(fill=False):
+
+    with ui.value_box(showcase=icon_svg("earlybirds")):
+        "Durchschnittliche Kaufkraft pro Person in Euro"
+
+        @render.text
+        def buying_average():
+            return "hellau7"
+
+    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+        "Kaufkraftindex pro Person"
+
+        @render.text
+        def buying_per_person():
+            return "hellau8"
+
+    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+        "Durchschnittliche Kaufkraft pro Haushalt in Euro"
+
+        @render.text
+        def buying_average_households():
+            return "hellau9"
+
+    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+        "Kaufkraftindex pro Haushalt"
+
+        @render.text
+        def buying_index_households():
+            return "hellau9"
 
 
 # === Styling (optional) ===
